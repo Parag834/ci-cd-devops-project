dece1fdf3e2785f8bb44f19526c9f69a1117230f
Ubuntu <ubuntu@ip-172-31-0-163.ap-south-1.compute.internal>
2025-12-15 11:26:23 +0000

    Enable live deployment with Flask on port 80

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("CI/CD Pipeline is working successfully!")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "CI/CD Project is LIVE on AWS EC2 🚀"
+
+app.run(host="0.0.0.0", port=80)
+
